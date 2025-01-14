f15745824693f4cf53c2326ada484f94672f85f5
mrubaish <mrubaish16@gmail.com>
2025-01-14 18:30:12 +0500

    some-code

---
+++ javascript class 2/index.js
@@ -0,0 +1,19 @@
+var HTML = prompt("What is the purpose of the <head> and <body> tags in an HTML document? Can you explain their differences?")
+
+alert("HTML")
+
+var CSS = prompt("How would you center a div horizontally and vertically on a webpage using CSS? ")
+
+alert("CSS")
+
+var JavaScript = prompt("Can you write a simple JavaScript function that calculates the square of a number and displays the result in the console?")
+
+alert("JavaScript")
+
+var Web = prompt("Why is it important to keep your code organized and well-commented?")
+
+alert("Web")
+
+var Responsive = prompt("What is the difference between px, em, and rem units in CSS, and when would you use each one?")
+
+alert("Responsive")
